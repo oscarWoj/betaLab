21b64f5c06472d64acf01cbf46d21cb60a2634f0
Oscar <56245006+oscarWoj@users.noreply.github.com>
2019-10-30 19:06:34 -0400

    added part of todays work

---
+++ daythirteen.playground/Contents.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+
+//Consolidation Day 1
+
+//Storing data
+
+var variable = "hello"
+
+let constant = "This is a constant"
+
+variable = "changed"
+
+//Constant = "doesnt work"
+
+/*
+ Two ways, constants and variables
+ */
+
+
+//Types of data
+
+var name = "Tim Mcgraw"
+var name2: String = "Tim Mcgraw"
+
+var integer = 2
+var integer2: Int = 2
+
+var double = 2.55
+var double2: Double = 2.55
+
+var bool = true
+var bool2:Bool = true
+
+
+var latitude: Double
+latitude = 36.166667
+
+var longitude: Float
+longitude = -86.783333
+longitude = -186.783333
+longitude = -1286.783333
+longitude = -12386.783333
+longitude = -123486.783333
+longitude = -1234586.783333
+
+/*
+ Use double whenever you can
+ */
+
+
+
